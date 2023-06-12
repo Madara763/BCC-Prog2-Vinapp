74ad832523664f8db0ec6290f9bf9284aeccf993
Madara763 <clashroyalemadara@gmail.com>
2023-06-12 00:14:08 -0300

    inicio

--- libvinapp.c
+++ libvinapp.c
@@ -11,73 +11,32 @@ int checaarg(int argc, char **argv){
     
     //testa o --help
     if(argc==2){    
-        if(strcmp(argv[1],"--help") == 0){
+        if((strcmp(argv[1],"-h") == 0) || (strcmp(argv[1],"--help") == 0) ){
             arghelp();
             return 0;
         }
     }
-         
-    //verifica se existem as flags -e ou -d
-    while(strcmp(argv[i], "-e") != 0 && strcmp(argv[i], "-d") != 0 && i < argc ){
-        i++;
-    }
-
-    //encode
-    if(strcmp(argv[i], "-e") == 0){
-        if (argc != 10){
-            argerror();
-            return 0;
-        }
-        
-        for(i=1; i<argc-1; i++){
-            if(strcmp(argv[i], "-b") == 0 && argv[i+1][0] != '-')      //livro cifras
-                *cifra=argv[i+1];            
-            
-            if(strcmp(argv[i], "-c") == 0 && argv[i+1][0] != '-')     //arquivos de chaves
-                *chaves=argv[i+1];
-            
-            if(strcmp(argv[i], "-m") == 0 && argv[i+1][0] != '-')     //mensagem original
-                *original=argv[i+1];
-            
-            if(strcmp(argv[i], "-o") == 0 && argv[i+1][0] != '-')     //mensagem codificada
-                *codificado=argv[i+1];
-            
-        }
-
-        if((*cifra!=NULL) && (*chaves!=NULL) && (*original!=NULL) && (*codificado!=NULL) )
-            return ENCODE;          
-    }
-    //decode
-    else if(strcmp(argv[i], "-d") == 0){    
-         if (argc != 8){
-            argerror();
-            return 0;
-        }
-        //armazena se o decode será com livro cifra ou arq chaves
-
-        for(i=1; i<argc-1; i++){
-            if(strcmp(argv[i], "-b") == 0 && argv[i+1][0] != '-')     //livro cifras
-                *cifra=argv[i+1];
-            
-            if(strcmp(argv[i], "-c") == 0 && argv[i+1][0] != '-')     //arquivos de chaves
-                *chaves=argv[i+1];
-            
-            if(strcmp(argv[i], "-o") == 0 && argv[i+1][0] != '-')     //mensagem decodificada
-                *decodificado=argv[i+1];
-            
-            if(strcmp(argv[i], "-i") == 0 && argv[i+1][0] != '-')     //mensagem codificada
-                *codificado=argv[i+1];
-            
-        }
-
-        if((*codificado!=NULL) && (*decodificado!=NULL) ){
-            if(*cifra!=NULL && *chaves == NULL)
-                return CIFRAS;
-            else if(*cifra==NULL && *chaves != NULL)
-                return CHAVES;
-        }
-    }//while 
+  
 
     argerror();
     return 0;
+}
+
+//imprime como devem ser os argumentos
+void arghelp(){
+    printf("\nArgumentos:");
+    printf("\n\t -i: insere/acrescenta um ou mais membros ao arquivo");
+    printf("\n\t -a: insere/acrescenta, mas só atualiza o membro caso seja mais");
+    printf(" recente que o ja adicionado");
+    printf("\n\t -x: extrai os membros indicados do arquivo. ");
+    printf("Se os membros nao forem indicados, extrai todo o arquivo.");
+    printf("\n\t -r: remove os membros indicados");
+    printf("\n\t -m target: move o membro indicado na linha de comando para ");
+    printf("imediatamente depois do membro target existete no arquivo.");
+
+    printf("\n\t -c arquivo de chave, opcional ao -b");
+
+    printf("\nAjuda:");
+    printf("\n\t -h ou --help");
+    printf("\nVina++ V1.0\tPor Davi Lazzarin\n");
 }
--- libvinapp.h
+++ libvinapp.h
@@ -4,11 +4,41 @@
     Criado por Davi Garcia Lazzarin
     Data: 06/06/2023
 */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #ifndef __LIBVINAPP__
 #define __LIBVINAPP__
 
+/*
+    Estrutura que armazenará as informações do 'archiver' .vpp
+*/
+typedef struct minfo{
+    char* nome;
+    char* nome_esp;
+    unsigned long long ini;
+    unsigned long pos;
+    dev_t ID;                 /* ID of device containing file */
+    mode_t modo;                /*File type and mode*/
+    uid_t uid;               /* User ID of owner */
+    gid_t gid;               /* Group ID of owner */
+    off_t size;              /* Total size, in bytes */
+    struct timespec mtim;    /* Time of last modification */
+    struct timespec ctim;    /* Time of last status change */
+} minfo;
 
+typedef struct m_nodo{
+    minfo *membro;
+    m_nodo *prox;
+    m_nodo *ante;
+} m_nodo;
+
+typedef struct jose{
+    unsigned long quant;
+    m_nodo *primeiro;
+    m_nodo *ultimo;
+} jose;
 
 
 #endif
